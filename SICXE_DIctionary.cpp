34f61825a67eade5a683299e3352726a31c294e0
unknown <smoro4198@sdsu.edu>
2021-04-18 13:12:18 -0700

    Dictionary placed in cpp

--- SICXE_DIctionary.cpp
+++ SICXE_DIctionary.cpp
@@ -2,5 +2,8 @@
 
 // default constructor initializes the dict
 SICXE_Dictionary::SICXE_Dictionary() {
-    mnemonics = {}; // <---add in here
+    mnemonics = {"ADD", "ADDF", "ADDR", "AND", "CLEAR", "COMP", "COMPF", "COMPR", "DIV", "DIVF", "DIVR", "FIX", "FLOAT"
+    , "HIO", "J", "JEQ", "JGT", "JLT", "JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL", "LDS", "LDT", "LDX", "LPS", "MUL", "MULF", "MULR"
+    , "NORM", "OR", "RD", "RMO", "RSUB", "SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB", "STCH", "STF", "STI", "STL", "STS", "STSW"
+    , "STT", "STX", "SUB", "SUBF", "SUBR", "SVC", "TD", "TIO", "TIX", "TIXR", "WD"}; // <---add in here
 }
--- a2.h
+++ a2.h
@@ -81,10 +81,7 @@ public:
 
 class SICXE_Dictionary {
 public:
-    set<string> mnemonics = {"ADD", "ADDF", "ADDR", "AND", "CLEAR", "COMP", "COMPF", "COMPR", "DIV", "DIVF", "DIVR", "FIX", "FLOAT"
-    , "HIO", "J", "JEQ", "JGT", "JLT", "JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL", "LDS", "LDT", "LDX", "LPS", "MUL", "MULF", "MULR"
-    , "NORM", "OR", "RD", "RMO", "RSUB", "SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB", "STCH", "STF", "STI", "STL", "STS", "STSW"
-    , "STT", "STX", "SUB", "SUBF", "SUBR", "SVC", "TD", "TIO", "TIX", "TIXR", "WD"};
+    set<string> mnemonics;
 
     SICXE_Dictionary();
 };
